be67fe8a68130ba8e1b3e68a0df1df661a53362c
Sofiân <bbksofian@msn.com>
2024-09-14 09:39:16 +0200

    Modifications #1

--- views/scripts/passwordToggle.js
+++ views/scripts/passwordToggle.js
@@ -13,7 +13,7 @@ function togglePasswordVisibility() {
         eyeIcon.innerHTML = '<i class="fas fa-eye-slash"></i>'; // Changer l'icône en œil barré
     } else {
         passwordInput.type = 'password';
-        eyeIcon.innerHTML = '<i class="fas fa-eye"></i>'; // Changer l'icône en œil
+        eyeIcon.innerHTML = '<i class="fas fa-eye"></i>'; // Changer l'icône en œilmmmmmm
     }
 }
 
